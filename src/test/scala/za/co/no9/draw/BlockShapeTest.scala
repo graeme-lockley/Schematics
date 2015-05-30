9e1d9256a1cf620c496d11185b235e3559bc8273
Graeme Lockley <graemel@no9.co.za>
2015-05-30 13:46:31 +0200

    Simplified the test a little.

--- src/test/scala/za/co/no9/draw/BlockShapeTest.scala
+++ src/test/scala/za/co/no9/draw/BlockShapeTest.scala
@@ -7,7 +7,7 @@ class BlockShapeTest extends FlatSpec with PropertyChecks with Matchers {
 	val theta = 0.001
 
 	"Given a block with no nested shape" should "still be able to layout without errors" in {
-		val shape = new BlockShape(List(), layoutPoint = (previous: LayedOutShape) => LayoutPoint(West(), At(Point(0, 0), Point(0, 0))))
+		val shape = new BlockShape(layoutPoint = _ => LayoutPoint(West(), At(Point(0, 0), Point(0, 0))))
 		val ls = new LayoutState(rotation = 30.0, scale = 2.0, translation = Point(0, 0))
 
 		val layedOutShape = shape.layout(initialLayedOutShape, ls)
@@ -19,8 +19,8 @@ class BlockShapeTest extends FlatSpec with PropertyChecks with Matchers {
 	"Given a nested shape" should "translate the layout correctly" in {
 		val shape = new BlockShape(
 			List(
-				new BoxShape(List(), layoutPoint = (previous: LayedOutShape) => LayoutPoint(West(), At(Point(50, 30), Point(0, 0))), width = 100, height = 20),
-				new BoxShape(List(), layoutPoint = (previous: LayedOutShape) => LayoutPoint(West(), At(previous.last(0).get.grips.east, Point(20, 0))), width = 100, height = 20)),
+				new BoxShape(layoutPoint = _ => LayoutPoint(West(), At(Point(50, 30), Point(0, 0))), width = 100, height = 20),
+				new BoxShape(layoutPoint = p => LayoutPoint(West(), At(p.last(0).get.grips.east, Point(20, 0))), width = 100, height = 20)),
 			layoutPoint = (previous: LayedOutShape) => LayoutPoint(West(), At(Point(0, 0), Point(0, 0))))
 		val ls = new LayoutState(rotation = 0.0, scale = 1.0, translation = Point(0, 0))
 
@@ -35,8 +35,8 @@ class BlockShapeTest extends FlatSpec with PropertyChecks with Matchers {
 	"Given a nested shape with a relative position for the bounding rectangle with a 30 degree rotation around absolute (0,0) and a scale of 2" should "translate the layout correctly" in {
 		val shape = new BlockShape(
 			List(
-				new BoxShape(List(), layoutPoint = (previous: LayedOutShape) => LayoutPoint(West(), At(Point(50, 30), Point(0, 0))), width = 100, height = 20),
-				new BoxShape(List(), layoutPoint = (previous: LayedOutShape) => LayoutPoint(West(), At(previous.last(0).get.grips.east, Point(20, 0))), width = 100, height = 20)),
+				new BoxShape(layoutPoint = _ => LayoutPoint(West(), At(Point(50, 30), Point(0, 0))), width = 100, height = 20),
+				new BoxShape(layoutPoint = p => LayoutPoint(West(), At(p.last(0).get.grips.east, Point(20, 0))), width = 100, height = 20)),
 			layoutPoint = (previous: LayedOutShape) => LayoutPoint(West(), At(Point(0, 0), Point(0, 0))))
 		val ls = new LayoutState(rotation = 30.0, scale = 2.0, translation = Point(0, 0))
 

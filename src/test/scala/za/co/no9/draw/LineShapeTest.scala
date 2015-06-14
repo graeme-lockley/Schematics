51cbbb34f99763b584a54757320392e264ab4a87
Graeme Lockley <graemel@no9.co.za>
2015-06-14 12:41:23 +0200

    More detail tests on the actual line points rather than just the rectangle

--- src/test/scala/za/co/no9/draw/LineShapeTest.scala
+++ src/test/scala/za/co/no9/draw/LineShapeTest.scala
@@ -10,9 +10,13 @@ class LineShapeTest extends FlatSpec with PropertyChecks with Matchers {
 		val line = new LineShape(_ => List(At(Point(0, 0), Point(0, 0)), At(Point(100, 0), Point(0, 0))))
 		val ls = new TX(rotation = 30.0, scale = 2.0, translation = Point(0, 0))
 
-		val layedOutShape = line.layout(initialLayedOutShape, ls)
+		val layedOutShape: LineLaidOutShape = line.layout(initialLayedOutShape, ls).asInstanceOf[LineLaidOutShape]
 
-		assert(pointEquals(layedOutShape.realBoundedRectangle.topLeft, Point(0.0, 0.0), theta))
-		assert(pointEquals(layedOutShape.realBoundedRectangle.bottomRight, Point(100.0, 0.0), theta))
+		layedOutShape.realBoundedRectangle.topLeft should equal(Point(0.0, 0.0))
+		layedOutShape.realBoundedRectangle.bottomRight should equal(Point(100.0, 0.0))
+
+		layedOutShape.absolutePoints.length should equal(2)
+		layedOutShape.absolutePoints(0) should equal(Point(0.0, 0.0))
+		layedOutShape.absolutePoints(1) should equal(Point(100.0, 0.0))
 	}
 }
